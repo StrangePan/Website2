2c5f0f31b6336a17277dc6fabf4c3215d2d2976a
Dan Andrus <danieleandrus@gmail.com>
2024-08-11 12:03:39 +0200

    Fix markdown resource file access from within jars

--- src/main/kotlin/me/strangepan/website/markdown/Markdown.kt
+++ src/main/kotlin/me/strangepan/website/markdown/Markdown.kt
@@ -1,7 +1,9 @@
 package me.strangepan.website.markdown
 
+import java.io.BufferedReader
 import java.io.File
 import java.io.FileNotFoundException
+import java.io.InputStreamReader
 import kotlinx.html.*
 import org.intellij.markdown.MarkdownParsingException
 import org.intellij.markdown.flavours.commonmark.CommonMarkFlavourDescriptor
@@ -10,10 +12,11 @@ import org.intellij.markdown.parser.MarkdownParser
 
 fun HTMLTag.markdown(filename: String) {
   unsafe {
-    val src = File(ClassLoader.getSystemResource("content/$filename").file).readText()
+    val inputStream = this.javaClass.getResourceAsStream("/content/$filename")
+    val markdown = BufferedReader(InputStreamReader(inputStream!!)).readText()
     val flavour = CommonMarkFlavourDescriptor()
-    val parsedTree = MarkdownParser(flavour).buildMarkdownTreeFromString(src)
-    val html = HtmlGenerator(src, parsedTree, flavour).generateHtml()
+    val parsedTree = MarkdownParser(flavour).buildMarkdownTreeFromString(markdown)
+    val html = HtmlGenerator(markdown, parsedTree, flavour).generateHtml()
       .substringAfter("<body>")
       .substringBeforeLast("</body>")
     raw(html)
