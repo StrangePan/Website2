deaa17c90c1a9efed0bb2ae8211e3acfe12356ed
Dan Andrus <danieleandrus@gmail.com>
2024-08-11 13:33:46 +0200

    Add variable for markdown library version

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@
 val kotlin_version: String by project
 val logback_version: String by project
 val kotlinx_html_version: String by project
+val jetbrains_markdown_version: String by project
 
 plugins {
   kotlin("jvm") version "2.0.10"
@@ -31,7 +32,7 @@ dependencies {
   implementation("io.ktor:ktor-server-html-builder-jvm")
   implementation("org.jetbrains.kotlinx:kotlinx-html-jvm:$kotlinx_html_version")
   implementation("org.jetbrains:kotlin-css-jvm:1.0.0-pre.129-kotlin-1.4.20")
-  implementation("org.jetbrains:markdown:0.7.3")
+  implementation("org.jetbrains:markdown:$jetbrains_markdown_version")
   implementation("io.ktor:ktor-server-netty-jvm")
   implementation("ch.qos.logback:logback-classic:$logback_version")
   testImplementation("io.ktor:ktor-server-test-host-jvm")
